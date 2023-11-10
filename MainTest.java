83ebba6f2edc6d1bbc61fc5b6c4b417988657e5b
David Yin <29613693+E137299@users.noreply.github.com>
2023-11-10 09:59:28 -0600

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -7,44 +7,14 @@ import org.junit.*;
 public class MainTest{
     @Test
     public void testAverage(){
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(2);
-        arr.add(3);
-        arr.add(3);
-        arr.add(3);
-        arr.add(4);
-        arr.add(4);
-        arr.add(4);
-        arr.add(4);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
+        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
         double result = Main.average(arr);
         Assert.assertEquals(3.6666666666666665, result,0.0001);
     }
 
     @Test
     public void testStandardDeviation(){
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(2);
-        arr.add(3);
-        arr.add(3);
-        arr.add(3);
-        arr.add(4);
-        arr.add(4);
-        arr.add(4);
-        arr.add(4);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
+        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
         double result = Main.standardDeviation(arr);
         Assert.assertEquals(1.2909944487358056, result,0.05);
 
@@ -52,22 +22,7 @@ public class MainTest{
 
         @Test
     public void testMode(){
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(2);
-        arr.add(3);
-        arr.add(3);
-        arr.add(3);
-        arr.add(4);
-        arr.add(4);
-        arr.add(4);
-        arr.add(4);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
-        arr.add(5);
+        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
         int result = Main.mode(arr);
         Assert.assertEquals(5, result);
 
