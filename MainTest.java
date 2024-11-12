296a71a193cfcbdef7070dabf1197b87ce0f8988
David Yin <29613693+E137299@users.noreply.github.com>
2024-11-12 08:53:51 -0600

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -1,28 +1,25 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-
+import java.util.*;
 import org.junit.*;
 
-public class MainTest{
+public class MainTest {
     @Test
-    public void testAverage(){
-        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
+    public void testAverage() {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5));
         double result = Main.average(arr);
-        Assert.assertEquals(3.6666666666666665, result,0.0001);
+        Assert.assertEquals(3.6666666666666665, result, 0.01);
     }
 
     @Test
-    public void testStandardDeviation(){
-        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
+    public void testStandardDeviation() {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5));
         double result = Main.standardDeviation(arr);
-        Assert.assertEquals(1.2909944487358056, result,0.05);
+        Assert.assertEquals(1.2909944487358056, result, 0.05);
 
     }
 
-        @Test
-    public void testMode(){
-        int[] arr = {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5};
+    @Test
+    public void testMode() {
+        ArrayList<Integer> arr = new ArrayList<>(List.of(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5));
         int result = Main.mode(arr);
         Assert.assertEquals(5, result);
 
